a29df2b1555890b0d075057bd4e327e0a8d487af
SrinuSara <162115806+SrinuSara@users.noreply.github.com>
2026-02-12 18:58:06 +0530

    version 11

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // <-- changed
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
